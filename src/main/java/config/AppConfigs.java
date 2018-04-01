6cc2fdf2506f2c50b753d0472ab3c619de92373a
Vitalii Netetskyi <vitaliynetetskiy@gmail.com>
2018-04-01 16:24:22 +0300

    Fixed Post deserialization

--- src/main/java/config/AppConfigs.java
+++ src/main/java/config/AppConfigs.java
@@ -1,5 +1,9 @@
 package config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import repo.InMemoryPostRepository;
@@ -11,5 +15,14 @@ public class AppConfigs {
     @Bean
     public PostRepository inmemoryPostRepository(){
         return new InMemoryPostRepository();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new ParameterNamesModule());
+        // make private fields visible to Jackson
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return objectMapper;
     }
 }
--- src/test/java/ObjectMappingTest.java
+++ src/test/java/ObjectMappingTest.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import config.AppConfigs;
 import junit.framework.TestCase;
 import model.Post;
 import org.junit.Test;
@@ -9,11 +10,14 @@ public class ObjectMappingTest {
 
     @Test
     public void postMapping() throws IOException {
+        final AppConfigs appConfigs = new AppConfigs();
+        final ObjectMapper objectMapper = appConfigs.objectMapper();
+
         final Post post1 = Post.of("some text");
         final Post post2 = new Post(null, "some text2", System.currentTimeMillis());
         final Post post3 = new Post(128L, "some text2", null);
         final Post post4 = new Post(128L, "some text2", System.currentTimeMillis());
-        final ObjectMapper objectMapper = new ObjectMapper();
+
         final String json1 = objectMapper.writeValueAsString(post1);
         final String json2 = objectMapper.writeValueAsString(post2);
         final String json3 = objectMapper.writeValueAsString(post3);
