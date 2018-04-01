79040c84b401bc526f666e50045e292e749d9bb5
Vitalii Netetskyi <vitaliynetetskiy@gmail.com>
2018-04-01 14:03:02 +0300

    Added object mapping test for Post model

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -1,12 +1,17 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor(onConstructor = @__(@JsonCreator))
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Post {
     private final Long id;
     private final String text;
-    private final long createdAtEpochMillis;
+    private final Long createdAtEpochMillis;
 
     public static Post of(String text) {
         return new Post(null, text, System.currentTimeMillis());
---
+++ src/test/java/ObjectMappingTest.java
@@ -0,0 +1,27 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import junit.framework.TestCase;
+import model.Post;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ObjectMappingTest {
+
+    @Test
+    public void postMapping() throws IOException {
+        final Post post1 = Post.of("some text");
+        final Post post2 = new Post(null, "some text2", System.currentTimeMillis());
+        final Post post3 = new Post(128L, "some text2", null);
+        final Post post4 = new Post(128L, "some text2", System.currentTimeMillis());
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String json1 = objectMapper.writeValueAsString(post1);
+        final String json2 = objectMapper.writeValueAsString(post2);
+        final String json3 = objectMapper.writeValueAsString(post3);
+        final String json4 = objectMapper.writeValueAsString(post4);
+
+        TestCase.assertEquals(post1, objectMapper.readValue(json1, Post.class));
+        TestCase.assertEquals(post2, objectMapper.readValue(json2, Post.class));
+        TestCase.assertEquals(post3, objectMapper.readValue(json3, Post.class));
+        TestCase.assertEquals(post4, objectMapper.readValue(json4, Post.class));
+    }
+}
